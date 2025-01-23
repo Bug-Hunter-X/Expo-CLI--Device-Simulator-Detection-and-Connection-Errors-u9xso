215fb31e3cedc49d84f6564e1b0dcb2e6257c36a
BugHunter-X <askzenai@gmail.com>
2025-01-23 04:40:07 +0100

    Add expoBug.js file

---
+++ expoBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI and attempting to run an app on a device or simulator that's not properly configured or connected.  It could manifest in several ways, including a generic error message or a failure to detect the device.
